98068e82c9d33d8a533e81493a85b1f6c15a9ddd
Scott Davis <scott@davisworld.org>
2010-01-15 13:30:44 -0700

    chapter 6

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -1,8 +1,11 @@
 dataSource {
 	pooled = true
-	driverClassName = "org.hsqldb.jdbcDriver"
-	username = "sa"
-	password = ""
+	//driverClassName = "org.hsqldb.jdbcDriver"
+	//username = "sa"
+	//password = ""
+	driverClassName = "com.mysql.jdbc.Driver"
+	username = "grails"
+	password = "server"
 }
 hibernate {
     cache.use_second_level_cache=true
@@ -14,19 +17,22 @@ environments {
 	development {
 		dataSource {
 			dbCreate = "create-drop" // one of 'create', 'create-drop','update'
-			url = "jdbc:hsqldb:mem:devDB"
+			//url = "jdbc:hsqldb:mem:devDB"
+			url = "jdbc:mysql://localhost:3306/racetrack_dev?autoreconnect=true"
 		}
 	}
 	test {
 		dataSource {
 			dbCreate = "update"
-			url = "jdbc:hsqldb:mem:testDb"
+			//url = "jdbc:hsqldb:mem:testDb"
+			url = "jdbc:mysql://localhost:3306/racetrack_dev?autoreconnect=true"
 		}
 	}
 	production {
 		dataSource {
 			dbCreate = "update"
-			url = "jdbc:hsqldb:file:prodDb;shutdown=true"
+			//url = "jdbc:hsqldb:file:prodDb;shutdown=true"
+			url = "jdbc:mysql://localhost:3306/racetrack_dev?autoreconnect=true"
 		}
 	}
 }
